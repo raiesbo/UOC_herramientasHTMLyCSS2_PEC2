f6a7f6baf8f224daba67e00e52b729f7dd51f8c5
raiesbo <raiesbo@gmail.com>
2022-05-01 11:33:21 +0200

    Implement timer

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -12,13 +12,31 @@ import * as bootstrap from 'bootstrap';
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+const daysTag = document.querySelector('.days__time');
+const hoursTag = document.querySelector('.hours__time');
+const minutesTag = document.querySelector('.min__time');
+const secondsTag = document.querySelector('.sec__time');
 
+const timeHandler = () => {
+  let lastDay = new Date('06.05.22');
+  let now = new Date();
 
-let days = 00;
-let hours = 00;
-let minutes = 00;
-let seconds = 00;
+  const diffTime = lastDay - now
+  const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+  const diffHours = Math.floor((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const diffMinutes = Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
+  const diffSeconds = Math.floor((diffTime % (1000 * 60)) / 1000);
+
+  daysTag.textContent = diffDays;
+  hoursTag.textContent = ensureDoubleDigit(diffHours);
+  minutesTag.textContent = ensureDoubleDigit(diffMinutes);
+  secondsTag.textContent = ensureDoubleDigit(diffSeconds);
+}
+
+const ensureDoubleDigit = (num) => {
+  return num < 10 ? `0${num}` : num;
+};
+
+setInterval(() => {
+  timeHandler();
+}, 1000);
